bcc305b71aa40ef97c6a2d78ef5f570700857b1c
Wayne-cyw <wayne.chiu0823@gmail.com>
2026-02-03 01:39:33 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ and evaluates its performance on a test dataset.
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
 
